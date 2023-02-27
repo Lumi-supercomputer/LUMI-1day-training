fbeb68ee95098fc3470f614bac864bb58e230124
Kurt Lust <Kurt.Lust@uantwerpen.be>
2023-02-27 16:17:30 +0100

    Some renaming of directories, and added EasyBuild exercise material.

--- make_tar.sh
+++ make_tar.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-tar -vcf exercises.tar --exclude "solution*.sh" --exclude "*.x" --exclude ".ptp*" --exclude "slurm*.out" LICENSE CPE_and_modules
+tar -vcf exercises.tar --exclude "solution*.sh" --exclude "*.x" --exclude ".ptp*" --exclude "slurm*.out" LICENSE CPE EasyBuild
